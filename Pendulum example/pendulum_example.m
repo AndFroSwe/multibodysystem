d8836838e95136ecb05e72b8fad3058f6c001838
andfroswe <gummianka88@gmail.com>
2016-03-25 09:56:57 +0100

    Cleaned up file structure. FIRST RELEASE

---
+++ Pendulum example/pendulum_example.m
@@ -0,0 +1,43 @@
+close all, clear all, clc
+addpath('../Utils')
+%% Main program
+% Draws a multibody sytem and animates the trajectories of the part of the
+% system
+
+% Input parameters 
+r = 0.3;
+
+% Must be set here to enable multiple objects 
+figure_handle = figure;
+hold on
+
+% Create masses in system
+mass1 = create_mass(0, 0, 0.1, 0.2, 'bl', 'rectangle');
+mass2 = create_mass(r*sin(1), -r*cos(1), 0.05, 0.1, 'y', 'ellipse');
+
+% Connections
+connection1 = create_connection(mass1, mass2);
+
+% Animation options
+fps = 30;   % Animation frames per second
+axis manual % Stop automatic axis size
+axis equal
+axis([-0.5 0.5 -0.5 0]) % Adjust as needed, to cover the desired area
+
+% Run simulation
+sim('pendulum')
+
+% Animation loop
+disp('Running simulation...')
+for i = 1:length(sim_theta.Data)
+    % Update positions
+    mass1.rotate_object(-3);
+    mass2.absolute_position(r*sin(sim_theta.Data(i)), -r*cos(sim_theta.Data(i)))
+    connection1.draw_line()
+    % Update frame
+    drawnow limitrate % Update rate 20 fps
+    drawnow % Update plot with new positions
+    pause(fps^-1)
+end
+        
+disp('Simulation done')
--- main.m
+++ main.m
@@ -1,43 +1,46 @@
 close all, clear all, clc
+addpath('../Utils')
+% Dont edit: Must be set here to enable multiple objects 
+figure_handle = figure;
+hold on
 %% Main program
 % Draws a multibody sytem and animates the trajectories of the part of the
 % system
 
-% Input parameters 
-r = 0.3;
+%%%%%%% Input parameters %%%%%%%%%%%
 
-% Must be set here to enable multiple objects 
-figure_handle = figure;
-hold on
+%%% Create masses in system %%%
 
-% Create masses in system
-mass1 = create_mass(0, 0, 0.1, 0.2, 'bl', 'rectangle');
-mass2 = create_mass(r*sin(1), -r*cos(1), 0.05, 0.1, 'y', 'ellipse');
+%%% Connections %%%
 
-% Connections
-connection1 = create_connection(mass1, mass2);
+%%% Run simulation %%%
+
+%%% Animation window options %%%
+xmax = 0.5;     
+xmin = -0.5;
+ymax = 0;
+ymin = -0.5;
+fps = 30;   % Animation frames per second
+animation_iteration = 100;          % Length of iteration
+
+%%%%%%% End of user input %%%%%%%%%
 
 % Animation options
-fps = 30;   % Animation frames per second
+% Dont edit
 axis manual % Stop automatic axis size
 axis equal
-axis([-0.5 0.5 -0.5 0]) % Adjust as needed, to cover the desired area
+axis([xmin xmax ymin ymax]) % Adjust as needed, to cover the desired area
 
-% Run simulation
-sim('pendulum')
-
-% Animation loop
+%%%%%%% Animation loop %%%%%%%
 disp('Running simulation...')
-for i = 1:length(sim_theta.Data)
-    % Update positions
-    mass1.rotate_object(-3);
-    mass2.absolute_position(r*sin(sim_theta.Data(i)), -r*cos(sim_theta.Data(i)))
-
-    connection1.draw_line()
+for i = 1:length(animation_iteration)
+    % Insert animation here %
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%
+    
     % Update frame
     drawnow limitrate % Update rate 20 fps
     drawnow % Update plot with new positions
     pause(fps^-1)
 end
-        
 disp('Simulation done')
